76804ff5eaaaa0e990d18fdca15918f415a92b85
isaac bustos <isaac.youthboard@futureindesign.org>
2017-12-11 10:16:51 -0700

    psudo coded what is supposed to be happening

---
+++ dpl_btrainteaser_6.rb
@@ -0,0 +1,21 @@
+#type exit or give input
+#gets iput to downcase
+#Counts all letters in input 
+#word wit highest count of repeated characters is stored somehow
+#returns stored word 
+#provide input or exit is repeated
+
+require 'pry'
+
+puts "letters menu"
+
+gets UserInput 
+    strip.to.downcase
+else exit
+     exit
+end
+
+gets ( UserInput )
+    UserInput.Counts
+
+
